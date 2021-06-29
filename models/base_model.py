0eb1fc3fa3d5ea984ba369687a9b5a45338d1faa
Oscar Angel <oa.angel26@gmail.com>
2021-06-29 17:16:11 +0000

    Comments added

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ class BaseModel:
     """create the BaseModel"""
 
     def __init__(self, *args, **kwargs):
+        """ Constructor of BaseModel wich initialices attribute values """
         if len(kwargs) > 0:
             for key, value in kwargs.items():
                 if key == 'created_at':
@@ -25,13 +26,16 @@ class BaseModel:
 
 
     def __str__(self):
+        """ Sepcial emthod wich returns in a string class, id, and object inf"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """ updates the public instance attribute with the current datetime """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        """ returns a dictionary containing all keys/values of the instance """
         dictionary = self.__dict__.copy()
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
