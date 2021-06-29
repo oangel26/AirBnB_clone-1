74fec03888f466074a78f48b512d56e321dbad75
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-06-29 16:19:55 -0500

    fixing 3 task

--- models/base_model.py
+++ models/base_model.py
@@ -3,13 +3,13 @@
 
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
     """create the BaseModel"""
 
     def __init__(self, *args, **kwargs):
         """ Constructor of BaseModel wich initialices attribute values """
-        from models import storage
         date_format = "%Y-%m-%dT%H:%M:%S.%f"
         if len(kwargs) > 0:
             for key, value in kwargs.items():
@@ -25,7 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Sepcial emthod wich returns in a string class, id, and object inf"""
@@ -34,9 +34,8 @@ class BaseModel:
 
     def save(self):
         """ updates the public instance attribute with the current datetime """
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of the instance """
