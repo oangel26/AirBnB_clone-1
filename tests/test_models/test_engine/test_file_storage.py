3ac0d3447ff1481929deca6db799539a447e62f4
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-06-30 22:10:27 -0500

    test_file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,11 +4,15 @@
 
 import unittest
 from models.engine.file_storage import FileStorage
+from models import storage
 
 
-class TestFileStorage(unittest.TestCase):
+class TestFileStorage(unittest,TestCase):
     """test class Amenity"""
 
     def setUp(self):
         """create a instance"""
         self.new_file_storage = FileStorage()
+
+if __name__ == "__main__":
+    unittest.main()
